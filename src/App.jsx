98ea1387cd6128f21f3006d8e3c1f4493c35b245
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-10-02 18:03:22 +0200

    export languages data; create first try app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,45 @@ import './App.css'
 
 
 
+
+
+
+import { useState } from "react";
+import languages from "./data/languages";
+import "./App.css";
+
+function App() {
+  // Stato: parte dal primo linguaggio
+  const [selectedId, setSelectedId] = useState(languages[0].id);
+
+  // Oggetto selezionato
+  const selected = languages.find(l => l.id === selectedId);
+
+  return (
+    <main className="app">
+      <h1>Learn Web development</h1>
+
+      {/* Lista bottoni */}
+      <div className="buttons">
+        {languages.map(lang => (
+          <button
+            key={lang.id}
+            onClick={() => setSelectedId(lang.id)}
+            className={selectedId === lang.id ? "btn active" : "btn"}
+          >
+            {lang.title}
+          </button>
+        ))}
+      </div>
+
+      {/* Card */}
+      <div className="card">
+        <h2>{selected.title}</h2>
+        <p>{selected.description}</p>
+      </div>
+    </main>
+  );
+}
+
+
 export default App
